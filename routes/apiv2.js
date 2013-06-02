c09bc9f05e08cbf6f2c4aa871d5fa8468c6bc5f9
Ray Garner <raygarner13@gmail.com>
2013-06-03 01:20:33 +0900

    created a new breed apiv2 still a WIP

--- routes/apiv2.js
+++ routes/apiv2.js
@@ -1,14 +1,47 @@
 var models = require('../models/models');
 
 exports.createData = function (req, res) {
-    var title = req.body.title;
-    var model = getModelInstance(req.params.type);
+    var subdoc = req.params.subdoc;
+    var type  = req.params.type;
+    var condition = {};
+    var model = getModelInstance(type);
     console.log(req.body);
-    var modelInstance = new model(req.body);
+    console.log(subdoc);
+    if(subdoc == undefined){
+        var modelInstance = new model(req.body);
 
-    modelInstance.save(function(err){
-        if(err)console.log(err);
-    })
+        //TODO:
+
+        //TODO:use the chain of responsibility pattern here to
+        //set datamodifiers
+        //TODO:make a class that decides which chain to delegate to
+        //based on request.
+        modelInstance.save(function(err){
+            if(err)console.log(err);
+            return sendSuccess(res);
+        })
+    }else{
+        model.findOne(condition,function(err,doc){
+            console.log(req.body);
+            //TODO:
+
+            //TODO:use the chain of responsibility pattern here to
+            //set datamodifiers
+            //TODO:make a class that decides which chain to delegate to
+            //based on request.
+            doc[subdoc].push(req.body);
+
+            model.save(function(err){
+                console.log(err);
+                return sendSuccess(res);
+
+            })
+        })
+
+
+    }
+
+
 
     /*
     var newData= new Petition(req.body);
@@ -16,7 +49,6 @@ exports.createData = function (req, res) {
         if (err)console.log(err);
     });
     */
-    return res.end(JSON.stringify({'success': 'true'}));
 }
 
 function getModelInstance(name){
@@ -32,19 +64,64 @@ exports.getData = function(req,res){
     var idPar = req.params.id;
     var model = getModelInstance(req.params.type);
     var condition = {};
-    if(idPar typeof StringValue){
-        idPar = idPar.lowercase;
-        if(idPar == "all"){
+    console.log(idPar);
+    //if api route id is undefined we are doing a sub doc query
+    if(idPar == undefined){
+        var field = req.params.field;
+        //javascript dynamically set property name of object literal.
+        condition[field] = req.params.query;
+    }else{
+        console.log(typeof(idPar))
+            if(idPar === "all"){
+            }else{
+                condition = {_id:idPar};
+            }
 
-        }else{
-            var field = req.params.field;
-            //javascript dynamically set property name of object literal.
-                condition = {field:req.params.query};
-        }
     }
+                    console.log(condition);
 
 
     model.find(condition,function(err,docs){
         return res.end(JSON.stringify(docs));
     })
+}
+
+var setFirstName ={register:"postText"};
+
+var dataModifier = function(data){
+    var next = null;
+
+    return{
+        setNext:function(stack){
+            next = stack;
+        },
+        setFirstName:function(data){
+            data.firstName = "BILLY";
+            return data;
+        },
+        setLastName:function(data){
+            data.lastName = "JEAN";
+            return data;
+        },
+        gotoNext:function(){
+            if(next){
+
+            }
+        }
+
+    }
+}
+function setAllFirstNames(data){
+    data.firstName = "Chain of Resp First Name ";
+    return data;
+}
+
+function setAllLastNames(data){
+    data.firstName = "Chain of Resp First Name ";
+    return data;
+}
+
+function sendSuccess(res){
+    return res.end(JSON.stringify({'success': 'true'}));
+
 }
